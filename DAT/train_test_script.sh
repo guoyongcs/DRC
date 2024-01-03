fa851934a0f03d10005764a06dc6af0bac8da94e
guoyongcs <guoyongcs@gmail.com>
2024-01-04 00:07:48 +0800

    Dual Regression for DAT

---
+++ DAT/train_test_script.sh
@@ -0,0 +1,5 @@
+# train SwinIR-DR
+CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH="./:${PYTHONPATH}" python -m torch.distributed.launch --nproc_per_node=4 --master_port=4321 basicsr/train.py -opt options/Train/train_DAT_x4_dr.yml --launcher pytorch --auto_resume
+
+# train SwinIR-DR on Set5
+PYTHONPATH="./:${PYTHONPATH}" python basicsr/test.py -opt options/Test/test_DAT_x4_dr.yml
