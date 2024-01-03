afc819b3b863ef7eb18ea31cace97039011d287d
guoyongcs <guoyongcs@gmail.com>
2024-01-04 00:04:57 +0800

    DRC for SwinIR

--- SwinIR/train_test_script.sh
+++ SwinIR/train_test_script.sh
@@ -1,8 +1,15 @@
-# train SwinIR-light-DRC
+# channel number search for SwinIR-light-DRC
+python main_search_swinir.py --opt_path options/swinir/search_swinir_sr_lightweight_drc.json --save_dir ../experiments/search_swinir_sr_lightweight_drc
+
+# channel pruning for SwinIR-light-DRC
+python main_pruning_swinir.py --opt_path options/swinir/pruning_swinir_sr_lightweight_drc.json --save_dir ../experiments/pruning_swinir_sr_lightweight_drc
+
+# finetune SwinIR-light-DRC
 python -m torch.distributed.launch --nproc_per_node=4 --master_port=1235 main_train_psnr_drc.py --opt options/swinir/train_swinir_sr_lightweight_drc.json --opt_teacher options/swinir/train_swinir_sr_classical.json --dist True
 
 # test SwinIR-light-DRC for Set5
 python main_test_swinir_drn.py --opt options/swinir/test_swinir_sr_lightweight_drc.json --task lightweight_sr_drc --folder_lq /data_root/dataset/sr/benchmark/Set5/LR_bicubic/X4 --folder_gt /data_root/dataset/sr/benchmark/Set5/HR --dataset Set5
+
 
 
 # train SwinIR-DR
